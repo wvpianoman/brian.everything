bfc42f1d5231fb1c682eb8232d8c85746dfe57eb
Flyin Brian <dbf.linux@gmail.com>
2025-07-04 07:33:33 -0400

    siduction update

--- 1-DISTRO/SIDUCTION/1-PACKAGES/1-PACKAGES.sh
+++ 1-DISTRO/SIDUCTION/1-PACKAGES/1-PACKAGES.sh
@@ -1,249 +1,104 @@
-#!/usr/bin/env bash
+#!/bin/bash
+### #!/usr/bin/env bash
 
-# Brian Francisco Packages
-# Mar 24, 2025
-
+# Brian Francisco
+# Personal use case packages
+# Jan 20 2024
 #   《˘ ͜ʖ ˘》
 
-# Tolga Erok for Brian
-# 19/4/2024
+# Install some software:
 
-# Assign a color variable based on the RANDOM number
-RED='\e[1;31m'
-GREEN='\e[1;32m'
-YELLOW='\e[1;33m'
-BLUE='\e[1;34m'
-CYAN='\e[1;36m'
-WHITE='\e[1;37m'
-ORANGE='\e[1;93m'
-NC='\e[0m'
+echo "Installing essential packages..."
 
-# Cache sudo credentials to avoid repeated password prompts
-sudo -v
+# sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
+nala update && nala install sublime-text
 
-# Function to handle errors
-handle_error() {
-    echo -e "${RED}Error occurred: $1${NC}"
-}
+# freeoffice
+sudo su
 
-# execute a command with error handling
-execute_command() {
-    "$@" || handle_error "Failed to execute: $*"
-}
+wget -qO- https://shop.softmaker.com/repo/linux-repo-public.key | gpg --dearmor > /etc/apt/keyrings/softmaker.gpg
+echo "deb [signed-by=/etc/apt/keyrings/softmaker.gpg] https://shop.softmaker.com/repo/apt stable non-free" > /etc/apt/sources.list.d/softmaker.list
+nala update && nala install softmaker-freeoffice-2024
 
-# Continue with the rest of the script
-echo -e "doing stuff..."
+# Megasync
+wget https://mega.nz/linux/repo/Debian_testing/amd64/megasync-Debian_testing_amd64.deb && nala install "$PWD/megasync-Debian_testing_amd64.deb"
+
+#Install gum : A tool for glamorous shell scripts. https://github.com/charmbracelet/gum
+
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+nala update && nala install gum
+
+# onlyoffice
+mkdir -p -m 700 ~/.gnupg
+gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+chmod 644 /tmp/onlyoffice.gpg
+sudo chown root:root /tmp/onlyoffice.gpg
+sudo mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
+
+echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+nala update && nala install onlyoffice-desktopeditors onlyoffice-desktopeditors-help
+
+#fileu
+# wget https://filelu.com/IO83v8fj9nbLQxAs/filelusync_amd64.zip
+# unzip filelusync_amd64.zip
+# cd filelusync_amd64
+# chmod +x install
+# ./install
+
+nala install blender blender-data gimp gimp-help-en inkscape boomaga digikam neochat telegram-desktop scribus scribus-template rclone rclone-browser flameshot fastfetch persepolis variety vlc simplescreenrecorder uget
+
+nala install cowsay dialog yad duf espeak espeak-ng fancontrol figlet fortune-mod fortunes fortunes-min pandoc fish uget aria2 hardinfo2 thefuck ocrmypdf ocrmypdf-doc pdfsandwich meld
+
+nala install acl akonadi-import-wizard aria2 attr autoconf automake bash-completion bc binutils btop busybox ca-certificates cifs-utils libcjson1 codec2 cookietool cowsay cron curl gir1.2-dbusglib-1.0 dconf-editor dialog direnv
+
+nala install dnsutils dolphin-plugins duf earlyoom easyeffects espeak espeak-ng fancontrol mbpfan fd-find ffmpeg ffmpegthumbnailer ffmpegthumbs figlet flatpak fortune-mod fortunes fortunes-min gdebi git gnupg2 grep
+
+nala install gtk2-engines-murrine murrine-themes uim-gtk{2.0,3} uim-gtk{2.0,3}-immodule uim-qt5 uim-qt5-immodule gtk2-engines haveged ibus-gtk4 jq
+
+nala install kate kdegraphics-thumbnailers libffi8 libffi-dev libfreeaptx0 libgc1 librabbitmq4 librabbitmq-dev librist4 libsodium23 libsodium-dev libtool libvdpau1 libvdpau-va-gl1 libxext6 llvm-16 lsd make meld libegl1-mesa
+
+nala install libgl{u,w}1-mesa mesa-va-drivers mesa-vulkan-drivers ublock-origin-doc webext-ublock-origin-firefox mpg123 nano neofetch neovim neovim-qt snmpd net-tools nftables openssh-{client,server} ostree p7zip p7zip-full p7zip-rar packagekit pandoc pip
+
+nala install pipewire-{audio,doc} pkg-config plasma-discover-backend-{flatpak,fwupd} plasma-firewall plocate powertop python3 python3-pip python3-setproctitle qrencode ripgrep rsync rygel sassc screen socat sshpass sxiv
+
+nala install tar terminator thefuck tumbler tumbler-plugins-extra ufw ugrep un{zip,rar} unrar-free variety vim virt-manager webext-ublock-origin-chromium wget wget2 wsdd xclip zip systemd-zram-generator zram-tools zstd
+
+korganizer kdepim-addons
+
+echo "Package installation completed."
+    sleep 3
 
 # read -n 1 -r -s -p $'Press enter to continue...\n'
 
-sudo apt install dialog zenity -y
 
-# essantial software pckages
-essential_packages=(
-    acl aria2 attr autoconf automake bash-completion bc binutils btop busybox ca-certificates cifs-utils libcjson1 codec2 cookietool 
-    cowsay cron curl gir1.2-dbusglib-1.0 dconf-editor dialog direnv dnsutils duf earlyoom easyeffects espeak espeak-ng fancontrol 
-    mbpfan fd-find figlet firmware-realtek flatpak fortune-mod fortunes fortunes-min gdebi git gnupg2 grep grub-customizer 
-    gstreamer1.0-{libav,vaapi} gstreamer1.0-plugins-{bad,base,good,rtp,ugly} gtk2-engines-murrine murrine-themes uim-gtk{2.0,3} 
-    uim-gtk{2.0,3}-immodule uim-qt5 uim-qt5-immodule gtk2-engines haveged ibus-gtk4 intel-media-va-driver iptables jq libavcodec-extra 
-    libffi8 libffi-dev libfreeaptx0 libgc1 librabbitmq4 librabbitmq-dev librist4 libsodium23 libsodium-dev libtool libvdpau1 libvdpau-va-gl1 
-    libxext6 llvm-16 lsd make libegl1-mesa libgl{u,w}1-mesa mesa-va-drivers mesa-vulkan-drivers ublock-origin-doc webext-ublock-origin-firefox 
-    mpg123 snmpd net-tools nftables openssh-{client,server} ostree p7zip p7zip-full p7zip-rar packagekit pandoc pip pipewire-{audio,doc} 
-    pkg-config plocate powertop python3 python3-pip python3-setproctitle qrencode ripgrep rsync rygel sassc screen socat sshpass sxiv tar 
-    terminator thefuck tlp tlp-rdw tlpui tumbler tumbler-plugins-extra ufw ugrep un{zip,rar} unrar-free   webext-ublock-origin-chromium wget 
-    wget2 wsdd xclip zip systemd-zram-generator zramswap-sysvinit-compat zram-tools zstd
-)
-# kde packages
-kde_packages=(
-    akonadi-import-wizard dolphin-plugins ffmpeg ffmpegthumbnailer ffmpegthumbs kate kdegraphics-thumbnailers kdepim-addons yakuake korganizer 
-    plasma-discover-backend-{flatpak,fwupd} plasma-firewall kate 
-)
 
-# gnome packages
-gnome_packages=(
-    breeze-icons breeze gnome-tweaks thunar-archive-plugin thunar thunar-volman thunar-docs thunar-shares-plugin numlockx spectacle kitty 
-    gnome-commander spacefm xfce4-terminal thunar-archive-plugin
-)
+sudo apt install earlyoom
+sudo systemctl enable --now earlyoom
 
-# software packages
-software_packages=(
-    blender blender-data ghostwriter gimp gimp-help-en krita inkscape boomaga digikam flameshot rclone rclone-browser rhythmbox scribus 
-    scribus-doc scribus-template shotwell simplescreenrecorder syncthing syncthing-gtk telegram-desktop uget vlc yakuake nheko nano 
-    fastfetch neovim neovim-qt variety virt-manager meld vim
-)
+echo "Installiong Software Packages"
 
-# utilities for file system access
-filesystem_utilities=(
-    btrfs-progs exfatprogs f2fs-tools hfsprogs hfsplus hfsutils jfsutils lvm2 nilfs-tools reiserfsprogs reiser4progs udftools xfsprogs disktype 
-    apfs-dkms apfsprogs libfsapfs-utils libfsapfs1 exfat-fuse btrfs-assistant
-)
 
-# utilities for file system access
-shells=(
-    zsh zsh-dbginfo zsh-autosuggestions zsh-syntax-highlighting antidote powerlevel10k powerline powerline-dbginfo rcm fish
-)
+echo "Package installation completed."
+    sleep 3
 
-# Install packages
-install_packages() {
-    echo -e "${ORANGE}$1${NC}"
-    sudo apt install -y --install-recommends "${@:2}" || handle_error "Failed to install: $1"
-    echo "Package installation completed."
-}
+# read -n 1 -r -s -p $'Press enter to continue...\n'
 
-# Install essential packages
-install_packages "Installing Essential Software Packages" "${essential_packages[@]}"
 
-# Install DE packages
-install_packages "Installing KDE Packages" "${kde_packages[@]}"
-#install_packages "Installing KDE Packages" "${gnome_packages[@]}"
-
-# Install Software Packages
-install_packages "Installing Software Packages""${software_packages[@]}"
-
-# Install filesystem utilities
-install_packages "Installing utilities for different file system access" "${shells[@]}"
-
-# Install Software Packages
-install_packages "Installing ZSH / FISH shells and Plug-ins""${software_packages[@]}"
-
-	# Install some fonts
-sudo apt install -y fonts-font-awesome fonts-noto-color-emoji xfonts-100dpi fonts-noto-color-emoji
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
+# Installing fonts
+sudo nala install fonts-font-awesome fonts-noto-color-emoji xfonts-100dpi fonts-noto-color-emoji
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 sudo unzip FiraCode.zip -d /usr/share/fonts
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
 sudo unzip Meslo.zip -d /usr/share/fonts
-
-wget https://github.com/tolgaerok/fonts-tolga/raw/main/WPS-FONTS.zip
-sudo unzip WPS-FONTS.zip -d /usr/share/fonts/wps-office
 
 # Reloading Font
 sudo fc-cache -vf
 
 # Removing zip Files
-rm ./FiraCode.zip ./Meslo.zip ./WPS-FONTS.zip
-
-	zip_file="Apple-Fonts-San-Francisco-New-York-master.zip"
-
-	# Check if the ZIP file exists
-	if [ -f "$zip_file" ]; then
-		# Remove existing ZIP file
-		sudo rm -f "$zip_file"
-		echo "Existing ZIP file removed."
-	fi
-
-	# Download the ZIP file
-	curl -LJO https://github.com/tolgaerok/Apple-Fonts-San-Francisco-New-York/archive/refs/heads/master.zip
-
-	# Check if the download was successful
-	if [ -f "$zip_file" ]; then
-		# Unzip the contents to the system-wide fonts directory
-		sudo unzip -o "$zip_file" -d /usr/share/fonts/
-
-		# Update font cache
-		sudo fc-cache -f -v
-
-		# Remove the ZIP file
-		rm "$zip_file"
-
-		display_message "[${GREEN}✔${NC}] Apple fonts installed successfully."
-		echo ""
-		gum spin --spinner dot --title "Re-thinking... 1 sec" -- sleep 2
-	else
-		display_message "[${RED}✘${NC}] Download failed. Please check the URL and try again."
-		gum spin --spinner dot --title "Stand-by..." -- sleep 2
-	fi
-
-sudo systemctl start thermald.service
-sudo systemctl status thermald.service
-
-sudo systemctl enable --now earlyoom
-
-	# Audio
-	[ -f /usr/bin/easyeffects ] && [ -f $HOME/.config/easyeffects/output/default.json ] && easyeffects -l default
-	[ -f /usr/bin/pulseeffects ] && [ -f $HOME/.config/PulseEffects/output/default.json ] && pulseeffects -l default
-
-	# Execute rygel to start DLNA sharing
-	/usr/bin/rygel-preferences
-
-speed-up-shutdown() {
-	display_message "${YELLOW}[*]${NC} Configure shutdown of units and services to 10s .."
-	sleep 1
-
-	# Configure default timeout to stop system units
-	sudo mkdir -p /etc/systemd/system.conf.d
-	sudo tee /etc/systemd/system.conf.d/default-timeout.conf <<EOF
-[Manager]
-DefaultTimeoutStopSec=10s
-EOF
-
-	# Configure default timeout to stop user units
-	sudo mkdir -p /etc/systemd/user.conf.d
-	sudo tee /etc/systemd/user.conf.d/default-timeout.conf <<EOF
-[Manager]
-DefaultTimeoutStopSec=10s
-EOF
-
-	display_message "${GREEN}[✔]${NC} Shutdown speed configured"
-	gum spin --spinner dot --title "Stand-by..." -- sleep 2
-}
-
-# Check GPU information
-    # Install firmware for AMD GPU
-    sudo apt update
-    sudo apt install firmware-amd-graphics -y
-    echo "AMD GPU firmware installed successfully."
-
-# Check GPU information
-gpu_info=$(lspci | grep -i 'VGA\|3D')
-if [[ -z $gpu_info ]]; then
-    echo "No GPU found."
-    exit 1
-fi
-
-# Check if NVIDIA GPU is present
-if [[ $gpu_info =~ "NVIDIA" ]]; then
-    # Check if NVIDIA drivers are already installed
-    if nvidia-smi &>/dev/null; then
-        read -r -p "NVIDIA drivers are already installed" -t 2 -n 1 -s
-        echo "."
-    else
-        # Install NVIDIA drivers
-        sudo apt update
-        sudo apt install nvidia-driver firmware-misc-nonfree -y
-        sudo apt install -y nvidia-driver
-        sudo bash -c 'echo -e "blacklist nouveau\noptions nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf'
-
-        # Path to the grub configuration file
-        grub_file="/etc/default/grub"
-
-        # Comment out the existing GRUB_CMDLINE_LINUX line
-        sed -i 's/^GRUB_CMDLINE_LINUX=/#&/' "$grub_file"
-
-        # Add the new GRUB_CMDLINE_LINUX line after the commented line
-        sed -i '/^#GRUB_CMDLINE_LINUX=/a GRUB_CMDLINE_LINUX="rhgb quiet rd.driver.blacklist=nouveau"' "$grub_file"
-
-        sudo update-grub
-
-        echo "NVIDIA drivers installed successfully."
-
-        # Run NVIDIA settings
-        sudo nvidia-settings
-    fi
-
-elif [[ $gpu_info =~ "AMD" ]]; then
-    # Install firmware for AMD GPU
-    sudo apt update
-    sudo apt install firmware-amd-graphics -y
-    echo "AMD GPU firmware installed successfully."
-
-else
-    # Install video acceleration for HD Intel i965
-    sudo apt update
-    sudo apt install xserver-xorg-video-intel
-    sudo apt install -y i965-va-driver libva-drm2 libva-x11-2 vainfo
-    echo "Video acceleration drivers installed successfully."
-fi
+rm ./FiraCode.zip ./Meslo.zip
 
 # Function to remove residual configuration files
 function remove_residual_config_files() {
@@ -279,7 +134,7 @@ sudo fstrim -av
 
 echo -e "\n\n----------------------------------------------"
 echo -e "|                                            |"
-echo -e "|               Setup Complete!              |"
+echo -e "|        Setup Complete! Enjoy MX!           |"
 echo -e "|                                            |"
 echo -e "----------------------------------------------\n\n"
 
