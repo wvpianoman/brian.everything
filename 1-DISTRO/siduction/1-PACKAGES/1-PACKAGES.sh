c98e1df4ff86e418dac517de213f29e99162fdb8
Brian F <dbf.linux@gmail.com>
2025-09-12 20:31:32 -0400

    update sid

--- 1-DISTRO/siduction/1-PACKAGES/1-PACKAGES.sh
+++ 1-DISTRO/siduction/1-PACKAGES/1-PACKAGES.sh
@@ -40,41 +40,26 @@ sudo apt install dialog zenity -y
 
 # essantial software pckages
 essential_packages=(
-    acl aria2 attr autoconf automake bash-completion bc binutils btop busybox ca-certificates cifs-utils libcjson1 codec2 cookietool 
-    cowsay cron curl gir1.2-dbusglib-1.0 dconf-editor dialog direnv dnsutils duf earlyoom easyeffects espeak espeak-ng fancontrol 
-    mbpfan fd-find figlet firmware-realtek flatpak fortune-mod fortunes fortunes-min gdebi git gnupg2 grep grub-customizer 
-    gstreamer1.0-{libav,vaapi} gstreamer1.0-plugins-{bad,base,good,rtp,ugly} gtk2-engines-murrine murrine-themes uim-gtk{2.0,3} 
-    uim-gtk{2.0,3}-immodule uim-qt5 uim-qt5-immodule gtk2-engines haveged ibus-gtk4 intel-media-va-driver iptables jq libavcodec-extra 
-    libffi8 libffi-dev libfreeaptx0 libgc1 librabbitmq4 librabbitmq-dev librist4 libsodium23 libsodium-dev libtool libvdpau1 libvdpau-va-gl1 
-    libxext6 llvm-16 lsd make libegl1-mesa libgl{u,w}1-mesa mesa-va-drivers mesa-vulkan-drivers ublock-origin-doc webext-ublock-origin-firefox 
-    mpg123 snmpd net-tools nftables openssh-{client,server} ostree p7zip p7zip-full p7zip-rar packagekit pandoc pip pipewire-{audio,doc} 
-    pkg-config plocate powertop python3 python3-pip python3-setproctitle qrencode ripgrep rsync rygel sassc screen socat sshpass sxiv tar 
-    terminator thefuck tlp tlp-rdw tlpui tumbler tumbler-plugins-extra ufw ugrep un{zip,rar} unrar-free   webext-ublock-origin-chromium wget 
-    wget2 wsdd xclip zip systemd-zram-generator zramswap-sysvinit-compat zram-tools zstd
+    acl aria2 attr autoconf automake bash-completion bc binutils btop busybox ca-certificates cifs-utils libcjson1 codec2 cookietool cowsay cron curl gir1.2-dbusglib-1.0 dconf-editor dialog direnv dnsutils duf earlyoom easyeffects espeak espeak-ng fancontrol mbpfan fd-find figlet firmware-realtek flatpak fortune-mod fortunes fortunes-min gdebi git gnupg2 grep grub-customizer gtk2-engines haveged ibus-gtk4 iptables jq ublock-origin-doc webext-ublock-origin-firefox mpg123 snmpd net-tools nftables openssh-{client,server} ostree p7zip p7zip-full p7zip-rar packagekit pandoc pip pipewire-{audio,doc} pkg-config plocate powertop python3 python3-pip python3-setproctitle qrencode ripgrep rsync rygel sassc screen socat sshpass sxiv tar terminator thefuck tlp tlp-rdw tlpui tumbler tumbler-plugins-extra ufw ugrep un{zip,rar} unrar-free webext-ublock-origin-chromium wget wget2 wsdd xclip zip systemd-zram-generator zramswap-sysvinit-compat zram-tools zstd yad
 )
 # kde packages
 kde_packages=(
-    akonadi-import-wizard dolphin-plugins ffmpeg ffmpegthumbnailer ffmpegthumbs kate kdegraphics-thumbnailers kdepim-addons yakuake korganizer 
-    plasma-discover-backend-{flatpak,fwupd} plasma-firewall kate 
+    akonadi-import-wizard dolphin-plugins ffmpeg ffmpegthumbnailer ffmpegthumbs kate kdegraphics-thumbnailers kdepim-addons yakuake korganizer plasma-discover-backend-{flatpak,fwupd} plasma-firewall kate 
 )
 
 # gnome packages
 gnome_packages=(
-    breeze-icons breeze gnome-tweaks thunar-archive-plugin thunar thunar-volman thunar-docs thunar-shares-plugin numlockx spectacle kitty 
-    gnome-commander spacefm xfce4-terminal thunar-archive-plugin
+    breeze-icons breeze gnome-tweaks thunar-archive-plugin thunar thunar-volman thunar-docs thunar-shares-plugin numlockx spectacle kitty gnome-commander spacefm xfce4-terminal thunar-archive-plugin
 )
 
 # software packages
 software_packages=(
-    blender blender-data ghostwriter gimp gimp-help-en krita inkscape boomaga digikam flameshot rclone rclone-browser rhythmbox scribus 
-    scribus-doc scribus-template shotwell simplescreenrecorder syncthing syncthing-gtk telegram-desktop uget vlc yakuake nheko nano 
-    fastfetch neovim neovim-qt variety virt-manager meld vim
+    blender blender-data ghostwriter gimp gimp-help-en krita inkscape boomaga digikam flameshot rclone rclone-browser rhythmbox scribus scribus-doc scribus-template shotwell simplescreenrecorder syncthing syncthing-gtk telegram-desktop uget vlc yakuake nheko nano fastfetch neovim neovim-qt variety meld #virt-manager
 )
 
 # utilities for file system access
 filesystem_utilities=(
-    btrfs-progs exfatprogs f2fs-tools hfsprogs hfsplus hfsutils jfsutils lvm2 nilfs-tools reiserfsprogs reiser4progs udftools xfsprogs disktype 
-    apfs-dkms apfsprogs libfsapfs-utils libfsapfs1 exfat-fuse btrfs-assistant
+    btrfs-progs exfatprogs f2fs-tools hfsprogs hfsplus hfsutils jfsutils lvm2 nilfs-tools reiserfsprogs reiser4progs udftools xfsprogs disktype apfs-dkms apfsprogs libfsapfs-utils libfsapfs1 exfat-fuse btrfs-assistant
 )
 
 # utilities for file system access
