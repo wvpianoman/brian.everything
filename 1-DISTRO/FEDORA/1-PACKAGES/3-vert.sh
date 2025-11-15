2f2571abaf62d247f05aa07062e0587a5b560e35
Alfred E. Neuman <dbf.linux@gmail.com>
2025-11-15 17:30:22 -0500

    fedora-cinnamon
    screw gnome and plasma

---
+++ 1-DISTRO/FEDORA/1-PACKAGES/3-vert.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Brian Francisco Packages
+# 12 May 2025
+
+# Clone the repository
+git clone https://github.com/VERT-sh/vert.git
+cd vert
+# Install dependencies
+bun i
+
+# Running Locally
+
+#To run the project locally, run bun dev.
---
+++ 1-DISTRO/LinuxMint/1-PACKAGES/1-PACKAGES.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+### #!/usr/bin/env bash
+
+# Brian Francisco
+# Personal use case packages
+# Jul 11 2025
+#   《˘ ͜ʖ ˘》
+
+# Install some software:
+
+sudo apt install apt -y
+
+echo "Installing packages not in repo..."
+
+# sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
+#pikman update && pikman install sublime-text
+
+# freeoffice
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+su -c "${__dir}/freeoffice.sh" root
+
+# wget -qO- https://shop.softmaker.com/repo/linux-repo-public.key | gpg --dearmor > /etc/apt/keyrings/softmaker.gpg
+# echo "deb [signed-by=/etc/apt/keyrings/softmaker.gpg] https://shop.softmaker.com/repo/apt stable non-free" > /etc/apt/sources.list.d/softmaker.list
+# pikman update && pikman install softmaker-freeoffice-2024
+
+# Megasync
+wget https://mega.nz/linux/repo/Debian_testing/amd64/megasync-Debian_testing_amd64.deb && sudo apt install "$PWD/megasync-Debian_testing_amd64.deb"
+
+#Install gum : A tool for glamorous shell scripts. https://github.com/charmbracelet/gum
+
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+# pikman update && pikman install gum
+
+# onlyoffice
+mkdir -p -m 700 ~/.gnupg
+gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+chmod 644 /tmp/onlyoffice.gpg
+sudo chown root:root /tmp/onlyoffice.gpg
+sudo mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
+
+echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+
+# grub-customizer
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt-get update
+sudo apt-get install grub-customizer
+
+# gcalendar
+sudo add-apt-repository ppa:slgobinath/gcalendar
+sudo apt update
+sudo apt install gcalendar
+
+# element
+sudo apt install -y wget apt-transport-https
+sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
+sudo apt update
+sudo apt install element-desktop
+
+# fish shell
+sudo add-apt-repository ppa:fish-shell/release-4
+sudo apt update
+sudo apt install fish
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
+
+sudo apt update && sudo apt install onlyoffice-desktopeditors onlyoffice-desktopeditors-help gum sublime-text fonts-crosextra-caladea
+
+#fileu
+# wget https://filelu.com/IO83v8fj9nbLQxAs/filelusync_amd64.zip
+# unzip filelusync_amd64.zip
+# cd filelusync_amd64
+# chmod +x install
+# ./install
+
+sudo apt install -y yad espeak espeak-ng fancontrol figlet fortune-mod fortunes fortunes-min uget hardinfo thefuck ocrmypdf ocrmypdf-doc pdfsandwich meld acl aria2 attr autoconf automake bash-completion bc binutils btop busybox ca-certificates cifs-utils libcjson1 codec2 cookietool cowsay cron curl gir1.2-dbusglib-1.0 dconf-editor dialog direnv dnsutils duf easyeffects mbpfan fd-find ffmpeg ffmpegthumbnailer ffmpegthumbs flatpak gdebi git gnupg2 grep yaru-cinnamon-theme-{gtk,icon} sox zenity synaptic plank lolcat vnstati
+
+sudo apt install -y  haveged ibus-gtk4 jq lsd make ublock-origin-doc webext-ublock-origin-firefox mpg123 nano neofetch neovim neovim-qt snmpd net-tools nftables openssh-{client,server} ostree p7zip p7zip-full p7zip-rar packagekit pandoc pip pipewire-{audio,doc} pkg-config plocate powertop python3 python3-pip python3-setproctitle qrencode ripgrep rsync rygel sassc screen socat sshpass sxiv tar terminator tumbler tumbler-plugins-extra ufw ugrep un{zip,rar} unrar-free variety vim webext-ublock-origin-chromium wget wget2 wsdd xclip zip systemd-zram-generator zram-tools zstd gparted kdeconnect meld #virt-manager
+
+sudo apt install
+
+sudo apt install hunspell-en-us hyphen-en-us libreoffice blender blender-data gimp gimp-help-en inkscape boomaga digikam neochat scribus scribus-template rclone rclone-browser flameshot vlc simplescreenrecorder megacmd obs-studio ocrmypdf duf gum figlet kitty pandoc gum thunar thunar-gtkhash thunar-archive-plugin thunar-media-tags-plugin thunar-font-manager thunar-volman thunar-megasync thunarx-python yakuake
+
+echo "Package installation completed."
+    sleep 3
+
+# read -n 1 -r -s -p $'Press enter to continue...\n'
+
+
+
+sudo apt install earlyoom
+sudo systemctl enable --now earlyoom
+
+echo "Installing Software Packages"
+
+
+echo "Package installation completed."
+    sleep 3
+
+# read -n 1 -r -s -p $'Press enter to continue...\n'
+
+
+# Installing fonts
+sudo apt install fonts-font-awesome xfonts-100dpi fonts-noto-color-emoji fonts-crosextra-caladea fonts-crosextra-carlito fonts-firacode fonts-noto-unhinted fonts-ubuntu-classic fonts-noto-mono
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Meslo.zip
+sudo unzip Meslo.zip -d /usr/share/fonts
+
+# Reloading Font
+sudo fc-cache -vf
+
+# Removing zip Files
+rm ./Meslo.zip
+
+# Function to remove residual configuration files
+function remove_residual_config_files() {
+    packages=$(dpkg -l | awk '/^rc/ { print $2 }')
+    if [ -n "$packages" ]; then
+        sudo dpkg -P $packages
+        echo "Residual configuration files removed."
+    else
+        echo "No residual configuration files found."
+    fi
+}
+
+# Function to clear systemd journal logs
+function clear_journal_logs() {
+    sudo journalctl --vacuum-time=7d
+    echo "Systemd journal logs cleared."
+}
+
+# Lets clean up
+echo -e "\n\n----------------------------------------------"
+echo -e "|     Let's clean up                         |"
+echo -e "----------------------------------------------\n\n"
+sudo update-grub
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+clear_journal_logs
+remove_residual_config_files
+
+echo -e "\n\n----------------------------------------------"
+echo -e "|     Let's clean up your SSD                 |"
+echo -e "----------------------------------------------\n\n"
+sudo fstrim -av
+
+echo -e "\n\n-------------------------------------------"
+echo -e "|                                         |"
+echo -e "|     Setup Complete! Enjoy LinuxMint     |"
+echo -e "|                                         |"
+echo -e "-------------------------------------------\n\n"
+
+exit 0
