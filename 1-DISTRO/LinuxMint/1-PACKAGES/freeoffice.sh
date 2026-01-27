c4efd7c76cc1949993414ccd7a8757117c0fd002
Insignificant_Human <dbf.linux@gmail.com>
2026-01-27 10:44:38 -0500

    updatin'

---
+++ 1-DISTRO/LinuxMint/1-PACKAGES/freeoffice.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+### #!/bin/bash
+
+sudo su
+
+wget -qO- https://shop.softmaker.com/repo/linux-repo-public.key | gpg --dearmor > /etc/apt/keyrings/softmaker.gpg
+echo "deb [signed-by=/etc/apt/keyrings/softmaker.gpg] https://shop.softmaker.com/repo/apt stable non-free" > /etc/apt/sources.list.d/softmaker.list
+
+sudo apt update && sudo apt install softmaker-freeoffice-2024
