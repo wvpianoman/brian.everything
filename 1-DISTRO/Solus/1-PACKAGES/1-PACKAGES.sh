7cfb548b51f2b6fea9371ec6041fa79a6fd0f3e4
iInsignificant_human <dbf.linux@gmail.com>
2026-02-27 21:04:16 -0500

    added  libayatana-appindicator

--- 1-DISTRO/Solus/1-PACKAGES/1-PACKAGES.sh
+++ 1-DISTRO/Solus/1-PACKAGES/1-PACKAGES.sh
@@ -44,7 +44,7 @@ sudo eopkg it dialog zenity -y
 
 # essantial software pckages
 essential=(
-    acl aria2 attr autoconf automake bash-completion bc binutils btop busybox perl-mozilla-ca python-certifi cjson curl dialog duf easyeffects espeak-ng fd findutils ffmpeg ffmpegthumbnailer rtl8852bu flatpak git zstd fan2go gnupg noto-sans-ttf grep gum ibus iptables jq lsd make meld libglu mpg123 nano fastfetch net-snmp nftables openssh-server openssh p7zip packagekit pandoc pip pipewire kpipewire wget httpie wsdd xclip zip zram-generator zram-generator-defaults plocate powertop python3 python-setproctitle qrencode ripgrep ripgrep-all rsync rygel sassc screen socat sshpass nsxiv tar terminator thefuck tlp thermald tumbler ufw gufw un{zip,rar} font-manager zed lolcat fortune-mod fan2go fan2go-dbginfo qt5ct git-lfs figlet yad hardinfo2 klassy klassy-dbginfo numbat numbat-dbginfo #starship #virt-manager
+    acl aria2 attr autoconf automake bash-completion bc binutils btop busybox perl-mozilla-ca python-certifi cjson curl dialog duf easyeffects espeak-ng fd findutils ffmpeg ffmpegthumbnailer rtl8852bu flatpak git zstd fan2go gnupg noto-sans-ttf grep gum ibus iptables jq lsd make meld libglu mpg123 nano fastfetch net-snmp nftables openssh-server openssh p7zip packagekit pandoc pip pipewire kpipewire wget httpie wsdd xclip zip zram-generator zram-generator-defaults plocate powertop python3 python-setproctitle qrencode ripgrep ripgrep-all rsync rygel sassc screen socat sshpass nsxiv tar terminator thefuck tlp thermald tumbler ufw gufw un{zip,rar} font-manager zed lolcat fortune-mod fan2go fan2go-dbginfo qt5ct git-lfs figlet yad hardinfo2 klassy klassy-dbginfo numbat numbat-dbginfo  libayatana-appindicator #starship #virt-manager
 )
 # kde packages
 kde_packages=(
